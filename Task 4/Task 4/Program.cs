56e277e54b3a14909d72a26d88872d28fc1eee91
arnabameer <arnabameer5@gmail.com>
2025-11-02 21:29:00 -0800

    23-51169-1

---
+++ Task 4/Task 4/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Enter first number: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter second number: ");
+int b = Convert.ToInt32(Console.ReadLine());
+a = a + b;
+b = a - b;
+a = a - b;
+Console.WriteLine($"After swapping: First number = {a}, Second number = {b}");
