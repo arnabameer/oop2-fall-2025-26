1106ce622bf93ea8c1d257a2b70e5583b999e4d0
arnabameer <arnabameer5@gmail.com>
2025-11-02 21:27:22 -0800

    23-51169-1

---
+++ Task 2/Task 2/Program.cs
@@ -0,0 +1,4 @@
+﻿double originalPrice = 250;
+double discountPercentage = 15;
+double discountedPrice = originalPrice - (originalPrice * discountPercentage / 100);
+Console.WriteLine($"The discounted price is: {discountedPrice}");
