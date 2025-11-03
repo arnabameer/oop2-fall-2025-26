677099aa6256fcb473ba677e4ea23377b93e6199
arnabameer <arnabameer5@gmail.com>
2025-11-02 21:24:13 -0800

    23-51169-1

---
+++ Task 1/Task 1/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Enter first score: ");
+int score1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter second score: ");
+int score2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter third score: ");
+int score3 = Convert.ToInt32(Console.ReadLine());
+double average = (score1 + score2 + score3) / 3.0;
+Console.WriteLine($"The average score is: {average}");
