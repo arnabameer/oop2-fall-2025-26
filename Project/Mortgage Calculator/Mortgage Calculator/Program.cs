c1ffbae09395f7941ecfa6e90e1dfdfae58d7a1d
arnabameer <arnabameer5@gmail.com>
2025-11-15 09:43:13 -0800

    23-51169-1

--- Project/Mortgage Calculator/Mortgage Calculator/Program.cs
+++ Project/Mortgage Calculator/Mortgage Calculator/Program.cs
@@ -1,29 +1,99 @@
 ﻿using System;
 using System.Globalization;
 
-Console.WriteLine("Mortgage Calculator");
+class Program
+{
+    static void Main()
+    {
+        double salary = 0;
+        int creditScore = 0;
+        bool criminalRecord = false;
 
-Console.Write("Enter the loan amount: ");
-double P = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("==Mortgage Calculator==");
 
-Console.Write("Enter the annual interest rate (in %): ");
-double annualInterestRate = Convert.ToDouble(Console.ReadLine());
+        
+        Console.Write("Enter your salary: ");
+        if (!double.TryParse(Console.ReadLine(), out salary))
+        {
+            Console.WriteLine("Invalid input! Try again.");
+            return;
+        }
 
-Console.Write("Enter the loan term (in years): ");
-int n = Convert.ToInt32(Console.ReadLine());    
+        
+        Console.Write("Enter your credit score: ");
+        if (!int.TryParse(Console.ReadLine(), out creditScore))
+        {
+            Console.WriteLine("Invalid input! Try again.");
+            return;
+        }
 
-double r = annualInterestRate / 100 / 12;
-int N = n * 12;
+        
+        if (creditScore <= 300)
+        {
+            Console.WriteLine("You are not eligible for the loan due to low credit score.");
+            return;
+        }
 
-double M = P * r * Math.Pow(1 + r, N) / (Math.Pow(1 + r, N) - 1);
+        
+        Console.Write("Do you have a criminal record? (true/false): ");
+        if (!bool.TryParse(Console.ReadLine(), out criminalRecord))
+        {
+            Console.WriteLine("Invalid input! Try again.");
+            return;
+        }
 
-double totalPayment = M * N;
-double totalInterest = totalPayment - P;
+        
+        if (criminalRecord == true)
+        {
+            Console.WriteLine("You are not eligible for the loan due to criminal record.");
+            return;
+        }
 
-CultureInfo BD = new CultureInfo("bn-BD");
-Console.WriteLine($"\nMonthly Payment: {M.ToString("C", BD)}");
-Console.WriteLine($"Total Payment: {totalPayment.ToString("C", BD)}");
-Console.WriteLine($"Total Interest Paid: {totalInterest.ToString("C", BD)}");
-Console.WriteLine("\nThank you for using the Mortgage Calculator!");
-Console.WriteLine("Press any key to exit...");
+        
+        Console.Write("Enter the loan amount: ");
+        if (!double.TryParse(Console.ReadLine(), out double P))
+        {
+            Console.WriteLine("Invalid input! Try again.");
+            return;
+        }
 
+        
+        if (P > salary * 2)
+        {
+            Console.WriteLine("You are not eligible for the loan because the loan amount is more than twice your salary.");
+            return;
+        }
+
+        
+        Console.Write("Enter the annual interest rate (in %): ");
+        if (!double.TryParse(Console.ReadLine(), out double annualInterestRate))
+        {
+            Console.WriteLine("Invalid input! Try again.");
+            return;
+        }
+
+        
+        Console.Write("Enter the loan term (in years): ");
+        if (!int.TryParse(Console.ReadLine(), out int n))
+        {
+            Console.WriteLine("Invalid input! Try again.");
+            return;
+        }
+
+        
+        double r = annualInterestRate / 100 / 12;
+        int N = n * 12;
+
+        double M = P * r * Math.Pow(1 + r, N) / (Math.Pow(1 + r, N) - 1);
+        double totalPayment = M * N;
+        double totalInterest = totalPayment - P;
+
+        CultureInfo BD = new CultureInfo("bn-BD");
+
+        Console.WriteLine($"\nMonthly Payment: {M.ToString("C", BD)}");
+        Console.WriteLine($"Total Payment: {totalPayment.ToString("C", BD)}");
+        Console.WriteLine($"Total Interest Paid: {totalInterest.ToString("C", BD)}");
+        Console.WriteLine("\nThank you for using the Mortgage Calculator!");
+        Console.WriteLine("Press any key to exit...");
+    }
+}
--- Project/Mortgage Calculator/Mortgage Calculator/obj/Debug/net9.0/Mortgage Calculator.AssemblyInfo.cs
+++ Project/Mortgage Calculator/Mortgage Calculator/obj/Debug/net9.0/Mortgage Calculator.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mortgage Calculator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd531622b57f24bf31c6d0ba6808a8289f6c2aae")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8bb34bcf2b807ff6530ea563e1ca728f873f24fd")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mortgage Calculator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mortgage Calculator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
